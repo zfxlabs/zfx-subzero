c87eebd5af2d6673b16f1b8856229a4051d04f4b
et4te <et4te@protonmail.com>
2022-01-03 16:56:42 -0400

    zfx-subzero: Removed old UTXO test code.

--- src/chain/alpha/tx.rs
+++ src/chain/alpha/tx.rs
@@ -2,59 +2,8 @@ use zfx_id::Id;
 
 use tai64::Tai64;
 
-// // A UTXO is an unspent transaction output, spendable by some public key
-// pub struct Output {
-//     pkh: Id,
-//     qty: u64,
-// }
-
-// impl Output {
-//     fn new(pkh: Id, qty: u64) -> Output {
-// 	Output { pkh, qty }
-//     }
-// }
-
-// // An input spends a UTXO based on the signature contained therein, we use an index here
-// // instead of a signature for testing purposes.
-// pub struct Input {
-//     i: u32,
-//     // signature: Signature,
-// }
-
-// // A transaction is constructed from inputs and outputs and has a type, which we use to
-// // create special types of transactions.
-// pub struct Tx {
-//     inputs: Vec<Input>,
-//     outputs: Vec<Output>,
-// }
-
-// impl Tx {
-//     pub fn new(inputs: Vec<Input>, outputs: Vec<Output>) -> Tx {
-// 	Tx { inputs, outputs }
-//     }
-
-//     /// A coinbase transaction is a transaction with only an output defined.
-//     pub fn coinbase(pkh: Id, qty: u64) {
-// 	let output = Output::new(pkh, qty);
-// 	Tx::new(vec![], vec![output])
-//     }
-
-//     pub fn spend(&self, qty: u64) -> Tx {
-// 	let total = self.outputs.clone().fold(0u64, |tot, out| tot + out.qty);
-// 	if qty > total {
-// 	    // error: insufficient balance
-// 	}
-// 	// if total - qty < fee {
-// 	    // error: insufficient balance to pay the transaction fee
-// 	// }
-// 	let inputs = self.outputs.fold(vec![], |is, out| {
-	    
-// 	});
-//     }
-// }
-
 // A transaction is constructed from inputs and outputs and has a type, which we use to
-// create special types of transactions.
+// create special types of transactions. Note: This is for testing only.
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct StakeTx {
     pub node_id: Id,
@@ -73,20 +22,18 @@ impl StakeTx {
     // pub fn stake(pkh, node_id, start_time, end_time, utxos: Vec<Output>) {}
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod test {
+    use super::*;
 
-//     use crate::util;
-    
-//     use std::net::SocketAddr;
+    // #[actix_rt::test]
+    // async fn test_spend() {
+    // 	// let (pk, sk) = Keypair::new();
+    // 	// let pkh = hash(pk);
 
-//     #[actix_rt::test]
-//     async fn test_stake_tx() {
-// 	let node_id = util::id_from_ip("127.0.0.1:1234".parse().unwrap());
-// 	let start_time = Tai64::now();
-// 	let end_time = Tai64::now().add(60 * 3);
-// 	let qty = 10000;
-// 	let _ = StakeTx::new(node_id, start_time, end_time, qty);
-//     }
-// }
+    // 	let utxo1 = Transaction::coinbase(pkh1, 1000);
+    // 	let utxo2 = Transaction::coinbase(pkh2, 1000);
+    // 	let utxo3 = Transaction::coinbase(pkh3, 1000);
+	
+    // }
+}
