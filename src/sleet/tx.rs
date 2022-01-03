613e351d3f55e670360da66b66b53b454c4ad4a0
et4te <et4te@protonmail.com>
2022-01-03 16:58:31 -0400

    zfx-subzero: `sleet` uses `alpha` transactions.

--- src/sleet/tx.rs
+++ src/sleet/tx.rs
@@ -1,15 +1,18 @@
+use crate::chain::alpha::Transaction;
+
+// Consensus representation of a transaction
 
 pub type TxHash = [u8; 32];
 
 #[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Tx {
     pub parents: Vec<TxHash>,
-    pub data: Vec<u8>,
+    pub inner: Transaction,
 }
 
 impl Tx {
-    pub fn new(parents: Vec<TxHash>, data: Vec<u8>) -> Self {
-	Tx { parents, data }
+    pub fn new(parents: Vec<TxHash>, inner: Transaction) -> Self {
+	Tx { parents, inner }
     }
 
     pub fn hash(&self) -> [u8; 32] {
