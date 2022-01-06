db7b615cf5288ce9611203cf2e9a59b6c587bcc9
et4te <et4te@protonmail.com>
2022-01-06 19:04:32 -0400

    zfx-subzero: Added `alpha` state application errors when applying a block.

--- src/chain/alpha/mod.rs
+++ src/chain/alpha/mod.rs
@@ -2,7 +2,29 @@ pub mod tx;
 pub mod block;
 pub mod state;
 
+mod initial_staker;
 mod alpha;
 
 pub use tx::*;
+pub use initial_staker::*;
 pub use alpha::*;
+
+use tx::{UTXOId, CoinbaseTx};
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Error {
+    InvalidCoinbaseInputs(CoinbaseTx),
+    InvalidCoinbaseOutputs,
+    InvalidUTXO(UTXOId),
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
