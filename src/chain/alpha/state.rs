80acf73d79270f6bf1b23f419c5afb9b890ffcdc
et4te <et4te@protonmail.com>
2022-01-06 19:05:47 -0400

    zfx-subzero: Improved `alpha` state application and added `utxo_id` tracking of unspent outputs.

--- src/chain/alpha/state.rs
+++ src/chain/alpha/state.rs
@@ -1,10 +1,16 @@
-use zfx_id::Id;
+use super::{Result, Error};
+use super::block::Block;
+use super::tx::{Transaction, UTXOId};
 
 use crate::colored::Colorize;
-use super::block::Block;
-use super::tx::Transaction;
+
+use zfx_id::Id;
 
 use tai64::Tai64;
+
+use std::collections::HashSet;
+
+use tracing::error;
 
 //-- State resulting from the application of blocks
 
@@ -15,39 +21,67 @@ pub struct State {
     pub height: u64,
     pub token_supply: u64,
     pub validators: Vec<(Id, u64)>,
+    pub utxo_ids: HashSet<UTXOId>,
 }
 
 //-- Apply transactions to the current state
 
 impl State {
     pub fn new() -> State {
-	State { height: 0, token_supply: 0, validators: vec![] }
+	State {
+	    height: 0,
+	    token_supply: 0,
+	    validators: vec![],
+	    utxo_ids: HashSet::new(),
+	}
     }
 
-    pub fn apply(&mut self, block: Block) {
+    pub fn apply(&mut self, block: Block) -> Result<()> {
 	self.height = block.height;
 	
 	// Compute the total supply
 	for tx in block.txs.iter() {
 	    match tx {
-		Transaction::StakeTx(stake_tx) => {
-		    self.token_supply += stake_tx.tx.sum();
+		Transaction::CoinbaseTx(tx) => {
+		    if tx.inputs().len() != 0 {
+			return Err(Error::InvalidCoinbaseInputs(tx.clone()));
+		    }
+		    if tx.outputs().len() != 1 {
+			return Err(Error::InvalidCoinbaseOutputs);
+		    }
+		    // Coinbase transactions create new tokens.
+		    self.token_supply += tx.tx.sum();
+		    // Coinbase transactions create new spendable outputs.
+		    let source = tx.hash().to_vec();
+		    let utxo_id_bytes = vec![source, vec![0]].concat();
+		    let utxo_id_encoded = bincode::serialize(&utxo_id_bytes).unwrap();
+		    let utxo_id = blake3::hash(&utxo_id_encoded).as_bytes().clone();
+		    self.utxo_ids.insert(utxo_id);
+		},
+		// Staking transactions spend coinbase transactions as inputs, thus they do
+		// not change the token supply.
+		Transaction::StakeTx(tx) => {
+		    // Staking transactions spend outputs (which therefore are no longer
+		    // spendable until the staking period ends). 
+		    for input in tx.inputs().iter() {
+			let utxo_id = input.utxo_id();
+			if !self.utxo_ids.remove(&utxo_id.clone()) {
+			    return Err(Error::InvalidUTXO(utxo_id));
+			}
+		    }
+
+		    // TODO: Staking transactions may spend less than the total within an
+		    // output, supplying the difference back to the staker (and producing
+		    // a spendable output).
+		    //
+		    // NOTE: Since we assume 'forever' staking here, we ignore lock time /
+		    // other concerns.
+		    self.validators.push((tx.node_id.clone(), tx.tx.sum()));
 		},
 	    }
 	}
 
-	// TODO: For testing purposes we make every validator stake forever
-	for tx in block.txs.iter() {
-	    match tx {
-		Transaction::StakeTx(stake_tx) => {
-		    // if stake_tx.start_time <= Tai64::now() && stake_tx.end_time >= Tai64::now() {
-		    //let w: f64 = percent_of(stake_tx.qty, self.total_tokens);
-		
-		    self.validators.push((stake_tx.node_id.clone(), stake_tx.tx.sum()));
-		    //}
-		}
-	    }
-	}
+	Ok(())
     }
 
     pub fn format(&self) -> String {
