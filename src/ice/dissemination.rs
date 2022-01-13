1a8ab0dec87f87f2f2214c061ec21edccca69532
Kristof Hetzl <kristof.hetzl@gmail.com>
2022-01-13 18:38:20 +0000

    Gossip message counter

--- src/ice/dissemination.rs
+++ src/ice/dissemination.rs
@@ -1,9 +1,17 @@
 use crate::zfx_id::Id;
 
 use actix::{Actor, Addr, Context, Handler};
+use blake2::digest::{Update, VariableOutput}; // for hash function
+use blake2::Blake2bVar;
+use futures::TryFutureExt;
+// for hash function
+use priority_queue::double_priority_queue::DoublePriorityQueue;
+use std::collections::HashMap;
 use tracing::debug;
 
-const GOSSIP_LIMIT: usize = 8; // Amount of gossip allowed to be passed
+const GOSSIP_LIMIT: usize = 3; // Amount of gossip allowed to be passed
+
+type GossipId = u64;
 
 #[derive(Debug, Clone, Message)]
 #[rtype(result = "GossipAck")]
@@ -23,13 +31,71 @@ pub struct Rumours {
     rumours: Vec<Gossip>,
 }
 
+//FXIME: Perhaps generalised version?
+pub struct PriorityMap {
+    c: GossipId, // Counter can be pretty simple, since this only DC calls this struct
+    h: HashMap<GossipId, Gossip>,
+    q: DoublePriorityQueue<GossipId, u64>,
+}
+
+impl PriorityMap {
+    pub fn new() -> PriorityMap {
+        PriorityMap { c: 0, h: HashMap::new(), q: DoublePriorityQueue::new() }
+    }
+
+    pub fn push(&mut self, g: Gossip) {
+        self.h.insert(self.c, g);
+        self.q.push(self.c, 0);
+        self.c += 1;
+    }
+
+    pub fn cleanup(&mut self, limit: u64) {
+        while self.has_over_limit(&limit) {
+            let (i, _p) = self.q.pop_max().unwrap();
+            self.h.remove(&i);
+        }
+    }
+
+    fn has_over_limit(&self, limit: &u64) -> bool {
+        match self.q.peek_max() {
+            None => false,
+            Some((_i, p)) => p >= limit,
+        }
+    }
+
+    pub fn take_n(&mut self, n: usize) -> Vec<Gossip> {
+        let mut v: Vec<(GossipId, u64)> = vec![];
+
+        for _ in 0..n {
+            match self.q.pop_min() {
+                None => break,
+                Some(kv) => v.push(kv),
+            }
+        }
+
+        for (id, p) in v.iter() {
+            self.q.push_increase(*id, p + 1);
+        }
+
+        v.iter().map(|(id, _)| self.h.get(&id).unwrap().clone()).collect()
+    }
+
+    pub fn empty(&self) -> bool {
+        self.q.len() == 0
+    }
+
+    pub fn len(&self) -> usize {
+        self.q.len()
+    }
+}
+
 pub struct DisseminationComponent {
-    gossip_queue: Vec<Gossip>,
+    rumours: PriorityMap,
 }
 
 impl DisseminationComponent {
     fn new() -> Self {
-        DisseminationComponent { gossip_queue: vec![] }
+        DisseminationComponent { rumours: PriorityMap::new() }
     }
 }
 
@@ -45,7 +111,7 @@ impl Handler<Gossip> for DisseminationComponent {
     type Result = GossipAck;
 
     fn handle(&mut self, msg: Gossip, _ctx: &mut Context<Self>) -> Self::Result {
-        self.gossip_queue.push(msg);
+        self.rumours.push(msg);
         GossipAck {}
     }
 }
@@ -54,14 +120,78 @@ impl Handler<GossipQuery> for DisseminationComponent {
     type Result = Rumours;
 
     fn handle(&mut self, msg: GossipQuery, _ctx: &mut Context<Self>) -> Self::Result {
-        let rumours_to_pass: usize = std::cmp::min(self.gossip_queue.len(), GOSSIP_LIMIT);
-        Rumours { rumours: self.gossip_queue[..rumours_to_pass].to_vec() }
+        Rumours { rumours: self.rumours.take_n(GOSSIP_LIMIT) }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::Rng;
+
+    const N: usize = 10; // Number of gossip messages
+    const NETWORK_SIZE: usize = 128; // Networks size in the test
+
+    #[actix_rt::test]
+    async fn test_pm_single() {
+        let mut pm: PriorityMap = PriorityMap::new();
+        assert!(pm.empty());
+        assert_eq!(pm.len(), 0);
+
+        let r = pm.take_n(1);
+        assert_eq!(0, r.len());
+
+        let slice = rand::thread_rng().gen::<[u8; 32]>(); // Random 32byte byte slice
+        let id = Id::new(&slice);
+        pm.push(Gossip::Leaver { id });
+        assert!(!pm.empty());
+        assert_eq!(pm.len(), 1);
+
+        // Cleanup with large number -> won't delete
+        pm.cleanup(10);
+        assert!(!pm.empty());
+        // Cleanup with 0 -> deletes all
+        pm.cleanup(0);
+        assert!(pm.empty());
+
+        pm.push(Gossip::Leaver { id });
+        // One item in the queue, one item in the response
+        let r2 = pm.take_n(1);
+        assert_eq!(r2.len(), 1);
+        let r3 = pm.take_n(200);
+        assert_eq!(r3.len(), 1);
+        pm.cleanup(10);
+        assert!(!pm.empty());
+        // Cleanup with 0 -> deletes all
+        pm.cleanup(2);
+        assert!(pm.empty());
+    }
+
+    #[actix_rt::test]
+    async fn test_pm_multi() {
+        let mut pm: PriorityMap = PriorityMap::new();
+
+        for _ in 0..100 {
+            let slice = rand::thread_rng().gen::<[u8; 32]>(); // Random 32byte byte slice
+            let id = Id::new(&slice);
+            pm.push(Gossip::Leaver { id });
+        }
+        assert_eq!(pm.len(), 100);
+        for _ in 0..10 {
+            pm.take_n(10);
+        }
+        assert_eq!(pm.len(), 100);
+        pm.cleanup(2);
+        // Nothing reached prio = 2, so nothing has been deleted.
+        // In other words, lowest prio is sent
+        assert_eq!(pm.len(), 100);
+
+        pm.take_n(50);
+        assert_eq!(pm.len(), 100);
+        // 50 items has been taken twice -> reached prio = 2 -> those will be deleted
+        pm.cleanup(2);
+        assert_eq!(pm.len(), 50);
+    }
 
     #[actix_rt::test]
     async fn test_simple_push_pull() {
@@ -80,4 +210,49 @@ mod tests {
         let Gossip::Leaver { id } = rumours.pop().unwrap();
         assert_eq!(id, stored_id);
     }
+
+    #[actix_rt::test]
+    async fn test_multi_push_pull() {
+        let dc = DisseminationComponent::new();
+        let dc_addr = dc.start();
+
+        let mut ids: Vec<Id> = vec![];
+
+        for i in 0..N {
+            let slice = rand::thread_rng().gen::<[u8; 32]>(); // Random 32byte byte slice
+            let id = Id::new(&slice);
+            ids.push(id);
+            match dc_addr.send(Gossip::Leaver { id: id.clone() }).await.unwrap() {
+                GossipAck {} => (),
+                _ => panic!("unexpected send result"),
+            }
+        }
+
+        let pulls = ((N / GOSSIP_LIMIT) as usize + 1);
+
+        for i in 0..pulls {
+            let Rumours { mut rumours } = dc_addr.send(GossipQuery {}).await.unwrap();
+            let len = rumours.len();
+            if len > GOSSIP_LIMIT {
+                panic!("unexpected rumours length {:?}", len);
+            }
+            if len == 0 {
+                panic!("no rumours could be pulled");
+            }
+            for g in rumours {
+                let Gossip::Leaver { id } = g;
+                assert!(ids.contains(&id));
+            }
+        }
+    }
+}
+
+// FIXME: Copied from zfx_id
+// This function is the replacement for `zfx_crypto`s `hash!` macro
+pub fn hash(input: &[u8]) -> [u8; 32] {
+    let mut hasher = Blake2bVar::new(32).unwrap();
+    hasher.update(input);
+    let mut buf = [0u8; 32];
+    hasher.finalize_variable(&mut buf).unwrap();
+    buf
 }
