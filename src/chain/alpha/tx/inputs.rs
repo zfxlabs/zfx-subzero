d5b6628c43048a4e8210f883854b0b1d8639d5d5
et4te <et4te@protonmail.com>
2022-01-14 08:18:50 -0400

    zfx-subzero: Fixed formatting, added mod changes.

--- src/chain/alpha/tx/inputs.rs
+++ src/chain/alpha/tx/inputs.rs
@@ -1,16 +1,15 @@
 use std::collections::HashSet;
 
-use std::ops::{Deref, DerefMut};
+use std::cmp::{Eq, Ord, Ordering, PartialEq};
 use std::hash::{Hash, Hasher};
-use std::cmp::{Ord, Eq, PartialEq, Ordering};
+use std::ops::{Deref, DerefMut};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Inputs<I: Eq + Hash> {
     pub inputs: HashSet<I>,
 }
 
-impl<I: Eq + Hash> Deref for Inputs<I>
-{
+impl<I: Eq + Hash> Deref for Inputs<I> {
     type Target = HashSet<I>;
 
     fn deref(&self) -> &'_ Self::Target {
@@ -18,8 +17,7 @@ impl<I: Eq + Hash> Deref for Inputs<I>
     }
 }
 
-impl<I: Eq + Hash> DerefMut for Inputs<I>
-{
+impl<I: Eq + Hash> DerefMut for Inputs<I> {
     fn deref_mut(&mut self) -> &'_ mut Self::Target {
         &mut self.inputs
     }
@@ -27,53 +25,48 @@ impl<I: Eq + Hash> DerefMut for Inputs<I>
 
 // Note: We only use this to hash the inputs for equality of a `tx`, not in the
 // hyperarc entries (otherwise conflict fails).
-impl<I: Eq + Hash + Ord + Clone> Hash for Inputs<I>
-{
+impl<I: Eq + Hash + Ord + Clone> Hash for Inputs<I> {
     fn hash<H: Hasher>(&self, state: &mut H) {
-	let mut v: Vec<I> = self.iter().cloned().collect();
-	v.sort();
-	v.hash(state);
+        let mut v: Vec<I> = self.iter().cloned().collect();
+        v.sort();
+        v.hash(state);
     }
 }
 
 impl<I: Eq + Hash + Ord + Clone> Eq for Inputs<I> {}
 
-impl<I: Eq + Hash + Ord + Clone> PartialEq for Inputs<I>
-{
+impl<I: Eq + Hash + Ord + Clone> PartialEq for Inputs<I> {
     fn eq(&self, other: &Self) -> bool {
-	let mut self_v: Vec<I> = self.iter().cloned().collect();
-	let mut other_v: Vec<I> = other.iter().cloned().collect();
-	self_v.sort();
-	other_v.sort();
-	self_v == other_v
+        let mut self_v: Vec<I> = self.iter().cloned().collect();
+        let mut other_v: Vec<I> = other.iter().cloned().collect();
+        self_v.sort();
+        other_v.sort();
+        self_v == other_v
     }
 }
 
-impl<I: Eq + Hash + Ord + Clone> Ord for Inputs<I>
-{
+impl<I: Eq + Hash + Ord + Clone> Ord for Inputs<I> {
     fn cmp(&self, other: &Self) -> Ordering {
-	let mut self_v: Vec<I> = self.iter().cloned().collect();
-	let mut other_v: Vec<I> = other.iter().cloned().collect();
-	self_v.sort();
-	other_v.sort();
-	self_v.cmp(&other_v)
+        let mut self_v: Vec<I> = self.iter().cloned().collect();
+        let mut other_v: Vec<I> = other.iter().cloned().collect();
+        self_v.sort();
+        other_v.sort();
+        self_v.cmp(&other_v)
     }
 }
 
-impl<I: Eq + Hash + Ord + Clone> PartialOrd for Inputs<I>
-{
+impl<I: Eq + Hash + Ord + Clone> PartialOrd for Inputs<I> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-	let mut self_v: Vec<I> = self.iter().cloned().collect();
-	let mut other_v: Vec<I> = other.iter().cloned().collect();
-	self_v.sort();
-	other_v.sort();
-	Some(self_v.cmp(&other_v))
+        let mut self_v: Vec<I> = self.iter().cloned().collect();
+        let mut other_v: Vec<I> = other.iter().cloned().collect();
+        self_v.sort();
+        other_v.sort();
+        Some(self_v.cmp(&other_v))
     }
 }
 
-impl<I: Eq + Hash + Ord + Clone> Inputs<I>
-{
+impl<I: Eq + Hash + Ord + Clone> Inputs<I> {
     pub fn new(inputs: Vec<I>) -> Self {
-	Inputs { inputs: inputs.iter().cloned().collect() }
+        Inputs { inputs: inputs.iter().cloned().collect() }
     }
 }
--- src/chain/alpha/tx/outputs.rs
+++ src/chain/alpha/tx/outputs.rs
@@ -1,16 +1,15 @@
 use std::collections::HashSet;
 
-use std::ops::{Deref, DerefMut};
+use std::cmp::{Eq, Ord, Ordering, PartialEq};
 use std::hash::{Hash, Hasher};
-use std::cmp::{Ord, Eq, PartialEq, Ordering};
+use std::ops::{Deref, DerefMut};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Outputs<O: Eq + Hash> {
     pub outputs: HashSet<O>,
 }
 
-impl<O: Eq + Hash> Deref for Outputs<O>
-{
+impl<O: Eq + Hash> Deref for Outputs<O> {
     type Target = HashSet<O>;
 
     fn deref(&self) -> &'_ Self::Target {
@@ -18,8 +17,7 @@ impl<O: Eq + Hash> Deref for Outputs<O>
     }
 }
 
-impl<O: Eq + Hash> DerefMut for Outputs<O>
-{
+impl<O: Eq + Hash> DerefMut for Outputs<O> {
     fn deref_mut(&mut self) -> &'_ mut Self::Target {
         &mut self.outputs
     }
@@ -28,52 +26,48 @@ impl<O: Eq + Hash> DerefMut for Outputs<O>
 // Assumption: We can hash the outputs because as long as the ordering is preserved
 // changing them constitutes a distinct transaction and thus must be handled as a
 // separate entity within the hypegraph.
-impl<O: Eq + Hash + Ord + Clone> Hash for Outputs<O>
-{
+impl<O: Eq + Hash + Ord + Clone> Hash for Outputs<O> {
     fn hash<H: Hasher>(&self, state: &mut H) {
-	let mut v: Vec<O> = self.iter().cloned().collect();
-	v.sort();
-	v.hash(state);
+        let mut v: Vec<O> = self.iter().cloned().collect();
+        v.sort();
+        v.hash(state);
     }
 }
 
 impl<O: Eq + Hash + Ord + Clone> Eq for Outputs<O> {}
 
-impl<O: Eq + Hash + Ord + Clone> PartialEq for Outputs<O>
-{
+impl<O: Eq + Hash + Ord + Clone> PartialEq for Outputs<O> {
     fn eq(&self, other: &Self) -> bool {
-	let mut self_v: Vec<O> = self.iter().cloned().collect();
-	let mut other_v: Vec<O> = other.iter().cloned().collect();
-	self_v.sort();
-	other_v.sort();
-	self_v == other_v
+        let mut self_v: Vec<O> = self.iter().cloned().collect();
+        let mut other_v: Vec<O> = other.iter().cloned().collect();
+        self_v.sort();
+        other_v.sort();
+        self_v == other_v
     }
 }
 
-impl<O: Eq + Hash + Ord + Clone> Ord for Outputs<O>
-{
+impl<O: Eq + Hash + Ord + Clone> Ord for Outputs<O> {
     fn cmp(&self, other: &Self) -> Ordering {
-	let mut self_v: Vec<O> = self.iter().cloned().collect();
-	let mut other_v: Vec<O> = other.iter().cloned().collect();
-	self_v.sort();
-	other_v.sort();
-	self_v.cmp(&other_v)
+        let mut self_v: Vec<O> = self.iter().cloned().collect();
+        let mut other_v: Vec<O> = other.iter().cloned().collect();
+        self_v.sort();
+        other_v.sort();
+        self_v.cmp(&other_v)
     }
 }
 
 impl<O: Eq + Hash + Ord + Clone> PartialOrd for Outputs<O> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-	let mut self_v: Vec<O> = self.iter().cloned().collect();
-	let mut other_v: Vec<O> = other.iter().cloned().collect();
-	self_v.sort();
-	other_v.sort();
-	Some(self_v.cmp(&other_v))
+        let mut self_v: Vec<O> = self.iter().cloned().collect();
+        let mut other_v: Vec<O> = other.iter().cloned().collect();
+        self_v.sort();
+        other_v.sort();
+        Some(self_v.cmp(&other_v))
     }
 }
 
-impl<O: Eq + Hash + Ord + Clone> Outputs<O>
-{
+impl<O: Eq + Hash + Ord + Clone> Outputs<O> {
     pub fn new(outputs: Vec<O>) -> Self {
-	Outputs { outputs: outputs.iter().cloned().collect() }
+        Outputs { outputs: outputs.iter().cloned().collect() }
     }
 }
