622a075c8c2f0da2d320db5af7c92b8a7f8f85d9
et4te <et4te@protonmail.com>
2022-01-18 19:09:39 -0400

    zfx-subzero: Implemented prelim output application.

--- src/cell/output.rs
+++ src/cell/output.rs
@@ -22,32 +22,28 @@ impl Output {
     }
 
     // Applies a series of unspent output states to form a new output state.
-    // pub fn apply<'a>(&'a self, outputs: Vec<Output>) -> Result<Output> {
-    // 	match self.cell_type {
-    // 	    CellType::Coinbase => {
-    // 		// Coinbase output cells are only valid at genesis, produce the same cells and have
-    // 		// no outputs.
-    // 		if outputs.len() != 0 {
-    // 		    return Err(Error::InvalidCoinbase);
-    // 		}
-    // 		if state.height == 0 {
-    // 		    Ok(self.clone())
-    // 		} else {
-    // 		    Err(Error::InvalidCoinbase)
-    // 		}
-    // 	    },
-    // 	    CellType::Transfer => {
-    // 		// Transfer output cells produce the same cells and do not have specific state.
-    // 		Ok(self.clone())
-    // 	    },
-    // 	    CellType::Stake => {
-    // 		// FIXME: Stake cells create new state designating a `node_id` who is responsible for
-    // 		// staking the capacity designated in the cell. Moreover the stake cell includes a
-    // 		// staking start and end time.
-    // 		Ok(self.clone())
-    // 	    },
-    // 	}
-    // }
+    pub fn apply<'a>(&'a self, outputs: Vec<Output>) -> Result<Output> {
+        match self.cell_type {
+            CellType::Coinbase => {
+                // Coinbase output cells are primitives which cannot be applied to other cells `data`.
+                if outputs.len() != 0 {
+                    return Err(Error::InvalidCoinbase);
+                }
+                Ok(self.clone())
+            }
+            CellType::Transfer => {
+                // Transfer output cells are primitives which cannot be applied to other cells `data`.
+                Ok(self.clone())
+            }
+            CellType::Stake => {
+                // Stake output cells are primitives which cannot be applied to other cells `data`.
+                if outputs.len() != 0 {
+                    return Err(Error::InvalidStake);
+                }
+                Ok(self.clone())
+            }
+        }
+    }
 
     // Verifies that the `data` in the current output is consistent with its consumed output cells.
     pub fn verify(&self, outputs: Vec<Output>) -> Result<()> {
@@ -70,9 +66,8 @@ impl Output {
                 if outputs.len() != 0 {
                     return Err(Error::InvalidStake);
                 }
-                // Stake operations must have a valid `node_id`.
-
-                // Note: We leave these two to make it easier to test the network.
+                // Note: We leave these temporarily to make it easier to test the network.
+                // TODO: Stake operations must have a valid `node_id`.
                 // TODO: Stake operations are only valid after the start time.
                 // TODO: Stake operations are only valid prior to the end time.
                 Ok(())
