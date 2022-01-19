e8db2c8dbf6c26cb4553f7549c7a0d1bf72698f5
Roman Damaskin <shendor1988@gmail.com>
2022-01-19 18:39:54 +0000

    Extracted node creation into another module and separated it from actix System in main function.

--- bin/main.rs
+++ bin/main.rs
@@ -1,18 +1,6 @@
-use zfx_subzero::alpha::Alpha;
-use zfx_subzero::client::Client;
-use zfx_subzero::hail::Hail;
-use zfx_subzero::ice::{self, Ice, Reservoir};
-use zfx_subzero::server::{Router, Server};
-use zfx_subzero::sleet::Sleet;
-use zfx_subzero::view::{self, View};
-use zfx_subzero::zfx_id::Id;
-use zfx_subzero::Result;
-
 use tracing::info;
 use tracing_subscriber;
-
-use actix::{Actor, Arbiter};
-use actix_rt::System;
+use zfx_subzero::{server, Result};
 
 use clap::{value_t, values_t, App, Arg};
 
@@ -24,33 +12,7 @@ use std::io::{BufReader, Read, Write};
 use std::net::SocketAddr;
 use std::path::Path;
 
-fn read_or_generate_keypair(node_id: String) -> Result<Keypair> {
-    let tmp_dir = vec!["/tmp/", &node_id].concat();
-    std::fs::create_dir_all(&tmp_dir).expect(&format!("couldn't create directory: {}", tmp_dir));
-    let keypair_path = vec![&tmp_dir[..], "/", &node_id, ".keypair"].concat();
-    match std::fs::File::open(keypair_path.clone()) {
-        Ok(mut file) => {
-            let mut buf_reader = BufReader::new(file);
-            let mut contents = String::new();
-            buf_reader.read_to_string(&mut contents)?;
-            info!("keypair => {:?}", contents.clone());
-            let keypair_bytes = hex::decode(contents).unwrap();
-            let keypair = Keypair::from_bytes(&keypair_bytes)?;
-            Ok(keypair)
-        }
-        Err(_) => {
-            let dir_path = vec!["/tmp/", &node_id].concat();
-            let mut csprng = OsRng {};
-            let keypair = Keypair::generate(&mut csprng);
-            let keypair_string = hex::encode(keypair.to_bytes());
-            info!("keypair => {:?}", keypair_string.clone());
-            std::fs::create_dir_all(dir_path).unwrap();
-            let mut file = std::fs::File::create(keypair_path)?;
-            file.write_all(keypair_string.as_bytes())?;
-            Ok(keypair)
-        }
-    }
-}
+use zfx_subzero::server::node;
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
@@ -90,101 +52,38 @@ fn main() -> Result<()> {
         .get_matches();
 
     let listener_ip =
-        value_t!(matches.value_of("listener-ip"), SocketAddr).unwrap_or_else(|e| e.exit());
-    let node_id = Id::from_ip(&listener_ip);
-    let node_id_str = hex::encode(node_id.as_bytes());
-
-    let keypair = match matches.value_of("keypair") {
-        Some(keypair_hex) => {
-            let dir_path = vec!["/tmp/", &node_id_str].concat();
-            let file_path = vec!["/tmp/", &node_id_str, "/", &node_id_str, ".keypair"].concat();
-            std::fs::create_dir_all(&dir_path)
-                .expect(&format!("Couldn't create directory: {}", dir_path));
-            let mut file = std::fs::File::create(file_path).unwrap();
-            file.write_all(keypair_hex.as_bytes()).unwrap();
-            let keypair_bytes = hex::decode(keypair_hex).unwrap();
-            Keypair::from_bytes(&keypair_bytes).unwrap()
-        }
-        None => read_or_generate_keypair(node_id_str.clone()).unwrap(),
-    };
-
+        value_t!(matches.value_of("listener-ip"), String).unwrap_or_else(|e| e.exit());
     let bootstrap_ips =
-        values_t!(matches.values_of("bootstrap-ip"), SocketAddr).unwrap_or_else(|e| e.exit());
-
-    let system = System::new();
-
-    let execution = async move {
-        // Initialise a view with the bootstrap ips and start its actor
-        let mut view = View::new(listener_ip);
-        view.init(bootstrap_ips);
-        let view_addr = view.start();
-
-        // Create the `ice` actor
-        let reservoir = Reservoir::new();
-        let ice = Ice::new(node_id, listener_ip, reservoir);
-        let ice_addr = ice.start();
-
-        // Create the 'client' actor
-        let client = Client::new();
-        let client_addr = client.start();
-
-        // Create the `hail` actor
-        let hail = Hail::new(client_addr.clone().recipient(), node_id);
-        let hail_addr = hail.start();
-
-        // Create the `sleet` actor
-        // FIXME: Sleet has to be initialised with the genesis utxo ids.
-        let sleet =
-            Sleet::new(client_addr.clone().recipient(), hail_addr.clone().recipient(), node_id);
-        let sleet_addr = sleet.start();
-
-        // Create the `alpha` actor
-        let db_path = vec!["/tmp/", &node_id_str, "/alpha.sled"].concat();
-        let alpha = Alpha::create(
-            Path::new(&db_path),
-            ice_addr.clone(),
-            sleet_addr.clone(),
-            hail_addr.clone(),
-        )
-        .unwrap();
-        let alpha_addr = alpha.start();
-
-        // Bootstrap the view
-        let view_addr_clone = view_addr.clone();
-        let ice_addr_clone = ice_addr.clone();
-        let alpha_addr_clone = alpha_addr.clone();
-
-        let bootstrap_execution = async move {
-            view::bootstrap(node_id, view_addr_clone.clone(), ice_addr_clone.clone()).await;
-            let view_addr_clone = view_addr_clone.clone();
-            let ice_addr_clone = ice_addr_clone.clone();
-            let ice_execution = async move {
-                // Setup `ice` consensus for establishing the liveness of peers
-                ice::run(node_id, ice_addr_clone, view_addr_clone, alpha_addr_clone).await;
-            };
-            let arbiter = Arbiter::new();
-            arbiter.spawn(ice_execution);
-        };
-
-        let listener_execution = async move {
-            // Setup the router
-            let router = Router::new(view_addr, ice_addr, alpha_addr, sleet_addr);
-            let router_addr = router.start();
-            // Setup the server
-            let server = Server::new(listener_ip, router_addr);
-            // Listen for incoming connections
-            server.listen().await.unwrap()
-        };
-
-        let arbiter = Arbiter::new();
-        arbiter.spawn(bootstrap_execution);
-        arbiter.spawn(listener_execution);
+        values_t!(matches.values_of("bootstrap-ip"), String).unwrap_or_else(|e| e.exit());
+    let keypair = match matches.value_of("keypair") {
+        Some(keypair_hex) => Some(String::from(keypair_hex)),
+        _ => None,
     };
 
-    let arbiter = Arbiter::new();
-    arbiter.spawn(execution);
+    let sys = actix::System::new();
+    sys.block_on(async move {
+        node::run(listener_ip, bootstrap_ips, keypair);
 
-    system.run().unwrap();
+        let sig = if cfg!(unix) {
+            use futures::future::FutureExt;
+            use tokio::signal::unix::{signal, SignalKind};
+
+            let mut sigint = signal(SignalKind::interrupt()).unwrap();
+            let mut sigterm = signal(SignalKind::terminate()).unwrap();
+
+            futures::select! {
+                _ = sigint.recv().fuse() => "SIGINT",
+                _ = sigterm.recv().fuse() => "SIGTERM"
+            }
+        } else {
+            tokio::signal::ctrl_c().await.unwrap();
+            "Ctrl+C"
+        };
+        info!(target: "sub-zero", "Got {}, stopping...", sig);
+
+        actix::System::current().stop();
+    });
+    sys.run().unwrap();
 
     Ok(())
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,5 +1,6 @@
 mod router;
 mod server;
+pub mod node;
 
 pub use router::*;
 pub use server::*;
