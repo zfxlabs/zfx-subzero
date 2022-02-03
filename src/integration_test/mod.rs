6359b3138aeed17dcd28b7badf3b70ba75f14e25
Roman Damaskin <shendor1988@gmail.com>
2022-02-03 10:14:32 +0000

    Renamed sleet_benchmark

--- src/integration_test/mod.rs
+++ src/integration_test/mod.rs
@@ -3,7 +3,7 @@
 mod hail_integration_test;
 mod integration_test;
 mod integration_test_runner;
-mod sleet_benchmark;
+mod cell_transfer_benchmark;
 mod stress_test;
 mod test_functions;
 mod test_model;
