4896d3e6c057f1545e195783f9f58928a3f0f34e
Roman Damaskin <shendor1988@gmail.com>
2022-02-03 10:31:25 +0000

    Fixed formatting issue.

--- src/integration_test/mod.rs
+++ src/integration_test/mod.rs
@@ -1,9 +1,9 @@
 #![cfg(test)]
 
+mod cell_transfer_benchmark;
 mod hail_integration_test;
 mod integration_test;
 mod integration_test_runner;
-mod cell_transfer_benchmark;
 mod stress_test;
 mod test_functions;
 mod test_model;
