e63d6ba82dbc7d192dfa5a2d791d8c3f1f0ce8a6
Roman Damaskin <shendor1988@gmail.com>
2022-02-20 18:53:07 +0000

    Included all other ITs in the runner.

--- src/integration_test/integration_test_runner.rs
+++ src/integration_test/integration_test_runner.rs
@@ -8,6 +8,7 @@ mod integration_test {
     use crate::Result;
     use std::thread::sleep;
     use std::time::Duration;
+    use crate::integration_test::cell_transfer_benchmark::run_cell_transfer_benchmark_test;
 
     #[actix_rt::test]
     async fn run_integration_test_suite() -> Result<()> {
@@ -22,6 +23,10 @@ mod integration_test {
         run_all_integration_tests().await?;
         sleep(Duration::from_secs(5));
         run_stress_test().await?;
+        sleep(Duration::from_secs(5));
+        run_hail_integration_test().await?;
+        sleep(Duration::from_secs(5));
+        run_cell_transfer_benchmark_test().await?;
 
         Result::Ok(())
     }
