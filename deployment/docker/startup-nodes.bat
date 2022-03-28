9808b2a16d679f8156c2e9c17572abfb0c0df745
Roman Damaskin <shendor1988@gmail.com>
2022-03-28 11:15:43 +0100

    Fixed some issues in deployment scripts

--- deployment/docker/startup-nodes.bat
+++ deployment/docker/startup-nodes.bat
@@ -1,9 +1,10 @@
-cd ../..
+SET ZFX_SCRIPTS_DIR=%~dp0
 
-echo "Start to build the docker image"
+cd ../..
+echo "Start to build the docker image for zfx-subzero node"
 call docker build -t zfx-subzero-node .
 
-cd deployment/docker
+cd %ZFX_SCRIPTS_DIR%
 echo "Run zfx-subzero nodes"
 call docker-compose stop
 call docker-compose rm -f
--- deployment/docker/startup-nodes.sh
+++ deployment/docker/startup-nodes.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+set -ex
+
+ZFX_ROOT="$(dirname $0)"
+cd $ZFX_ROOT
+ZFX_SCRIPTS_DIR="$(pwd $0)"
+
 cd ../..
 echo "Start to build the docker image for zfx-subzero node"
-docker build -t zfx-performance-test-node .
+docker build -t zfx-subzero-node .
 
-cd deployment/docker || exit
+cd $ZFX_SCRIPTS_DIR
 echo "Run zfx-subzero nodes"
 docker-compose stop
 docker-compose rm -f
