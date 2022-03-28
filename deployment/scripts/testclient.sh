c0c95d652c547649cb4d4d0da578aba15226a042
Roman Damaskin <shendor1988@gmail.com>
2022-03-28 19:02:35 +0100

    Changed paths to cert in some scripts.

--- deployment/scripts/testclient.sh
+++ deployment/scripts/testclient.sh
@@ -15,7 +15,7 @@ if [[ -z "$USE_TCP" ]]; then
        --peer 12My22AzQQosboCy6TCDFkTQwHTSuHhFN1VDcdDRPUe3H8j3DvY@127.0.0.1:1234 \
        --keypair ad7f2ee3958a7f3fa2c84931770f5773ef7694fdd0bb217d90f29a94199c9d7307ca3851515c89344639fe6a4077923068d1d7fc6106701213c61d34ef8e9416 \
        --cell-hash b5fba12b605e166987f031c300e33969e07e295285a3744692f326535fba555e \
-       --use-tls -p test-certs/test.key -c test-certs/test.crt\
+       --use-tls -p deployment/test-certs/test.key -c deployment/test-certs/test.crt\
        "${@}"
 else
 
