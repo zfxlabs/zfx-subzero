9915c34bef324978296deca6c28d85108842f7ee
et4te <et4te@protonmail.com>
2022-06-17 09:45:29 -0400

    p2p: Made the `connection_handler` generic over requests / responses.

--- src/p2p/connection_handler.rs
+++ src/p2p/connection_handler.rs
@@ -5,18 +5,20 @@ use super::response_handler::ResponseHandler;
 
 use crate::channel::Channel;
 
+use crate::protocol::{Request, Response};
+
 #[derive(Clone)]
-pub struct ConnectionHandler {
-    request: Request,
+pub struct ConnectionHandler<Req: Request, Rsp: Response> {
+    request: Req,
     send_timeout: Duration,
-    response_handler: Arc<dyn ResponseHandler>,
+    response_handler: Arc<dyn ResponseHandler<Rsp>>,
 }
 
-impl ConnectionHandler {
+impl<Req: Request, Rsp: Response> ConnectionHandler<Req, Rsp> {
     pub fn new(
-        request: Request,
+        request: Req,
         send_timeout: Duration,
-        response_handler: Arc<dyn ResponseHandler>,
+        response_handler: Arc<dyn ResponseHandler<Rsp>>,
     ) -> Self {
         ConnectionHandler { request, send_timeout, response_handler }
     }
@@ -28,7 +30,7 @@ impl ConnectionHandler {
         let request = self.request.clone();
         let response_handler = self.response_handler.clone();
         let send_timeout = self.send_timeout.clone();
-        let mut channel: Channel<Request, Response> =
+        let mut channel: Channel<Req, Rsp> =
             Channel::wrap(connection.state.connection_stream).unwrap();
         let (mut sender, mut receiver) = channel.split();
         Box::pin(async move {
