590c7e6db163317951ff71f184e9e40c17dc92e3
Andras Horvath <horvand@users.noreply.github.com>
2022-06-28 09:39:59 +0200

    Rustdoc for Hail

--- src/hail/mod.rs
+++ src/hail/mod.rs
@@ -1,8 +1,10 @@
 //! Hail is a consensus algorithm based on Snowman but augmented with cryptographic sortition.
 //!
+//! `hail` is a block based consensus algorithm based on Avalanche which uses directed acylic graphs and VRFs.
+//! It is the primary consensus mechanism for all block based chains defined within the `zero.fx` network.
 //! It is specialised to blocks and ensures that no two conflicting blocks can be accepted at the same height.
 //! Similar to [`sleet`][crate::sleet], no inner verification of the block contents nor execution
-//! of state transitions is done besides on alpha primitive cells (such as staking cells).
+//! of state transitions is done besides on [alpha][crate::alpha] primitive cells (such as staking cells).
 
 pub mod block;
 mod committee;
@@ -12,12 +14,14 @@ mod hail;
 mod vertex;
 
 pub use hail::*;
+pub use vertex::Vertex;
 
 use crate::alpha;
 use crate::alpha::block::Block;
 use crate::alpha::types::{BlockHash, BlockHeight};
 use crate::graph;
 
+/// The module's error type
 #[derive(Debug, Eq, PartialEq)]
 pub enum Error {
     ActixMailboxError,
@@ -65,4 +69,5 @@ impl std::fmt::Display for Error {
     }
 }
 
+/// The module's result type
 pub type Result<T> = std::result::Result<T, Error>;
